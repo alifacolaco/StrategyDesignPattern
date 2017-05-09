dffa1ccbd99e57de75f61a13f1081c24ea8cbf1f
alifacolaco <alifacolaco@gmail.com>
2017-05-09 10:13:12 +0530

    Updated Strategy.java

--- src/com/math/Strategy.java
+++ src/com/math/Strategy.java
@@ -2,6 +2,7 @@ package com.math;
 
 /**
  * Created by SG0227814 on 5/3/2017.
+ * I'm testing form build on cimmit in VCS
  */
 public interface Strategy {
     public int doOperation(int num1, int num2);
